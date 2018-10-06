e9cbce282190352739c031f7bb1f6121e56bc62b
nickfp1985 <nfpeddie@gmail.com>
2018-10-06 09:35:36 -0700

    made some basic layout and styling changes to the budget page and component

--- client/src/pages/Budget.js
+++ client/src/pages/Budget.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import styled from "react-emotion";
 import Grid from "@material-ui/core/Grid";
+import BudgetItem from "../components/BudgetItem";
 
 const Wrapper = styled('div')(
     {
         border: '5px solid #092d63',
-        textAlign: 'center',
+        itemAlign: 'center',
         margin: '20px 0'
+    }
+)
+
+const Category = styled('div')(
+    {
+        border: '2px solid #092d63'
+    }
+)
+
+const SaveButton = styled('button')(
+    {
+        padding: 15,
+        textTransform: 'uppercase',
+        border: '1px solid black',
+        borderRadius: 5
     }
 )
 
 class Budget extends Component {
 
     state = {
-        data: {}
+        data: {},
+        category: '',
+        amount: ''
     }
 
     componentDidMount() {
@@ -29,13 +47,65 @@ class Budget extends Component {
                         <h1>
                             BUDGET PAGE
                         </h1>
-                    </Wrapper>
-                </Grid>
-                <Grid item lg={6} md={6} sm={10} xs={12}>
-                    <Wrapper>
-                        <h1>
-                            BUDGET PAGE
-                        </h1>
+                        <Category>
+                            <BudgetItem
+                                name="category"
+                                label="Category"
+                                onChangeFn={this.handleChange}
+                                value={this.state.name}
+                            />
+                            <BudgetItem
+                                name="amount"
+                                label="Amount"
+                                onChangeFn={this.handleChange}
+                                value={this.state.amount}
+                            />
+                        </Category>
+                        <Category>
+                            <BudgetItem
+                                name="category"
+                                label="Category"
+                                onChangeFn={this.handleChange}
+                                value={this.state.name}
+                            />
+                            <BudgetItem
+                                name="amount"
+                                label="Amount"
+                                onChangeFn={this.handleChange}
+                                value={this.state.amount}
+                            />
+                        </Category>
+                        <Category>
+                            <BudgetItem
+                                name="category"
+                                label="Category"
+                                onChangeFn={this.handleChange}
+                                value={this.state.name}
+                            />
+                            <BudgetItem
+                                name="amount"
+                                label="Amount"
+                                onChangeFn={this.handleChange}
+                                value={this.state.amount}
+                            />
+                        </Category>
+                        <Category>
+                            <BudgetItem
+                                name="category"
+                                label="Category"
+                                onChangeFn={this.handleChange}
+                                value={this.state.name}
+                            />
+                            <BudgetItem
+                                name="amount"
+                                label="Amount"
+                                onChangeFn={this.handleChange}
+                                value={this.state.amount}
+                            />
+                        </Category>
+                        <SaveButton onClick={this.handleSubmit}>
+                            Save
+                        </SaveButton>
                     </Wrapper>
                 </Grid>
 
