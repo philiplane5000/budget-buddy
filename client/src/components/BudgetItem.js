c67e976ee786d1c92fd15bc22e40bc28a924ee16
nickfp1985 <nfpeddie@gmail.com>
2018-10-08 19:43:15 -0700

    style changes made to the budget page and budgetitem component to take up more space and match color scheming of the home page

--- client/src/components/BudgetItem.js
+++ client/src/components/BudgetItem.js
@@ -1,27 +1,35 @@
 import React from "react";
 import styled from "react-emotion";
 
+const BudgetContent = styled("div")({
+  height: 65,
+  width: 350,
+  backgroundColor: "#2fc4a6",
+  color: 'whitesmoke',
+  paddingTop: '5px'
+});
+
 const CategoryInput = styled('input')({
-  padding: 5,
-  border: 'none',
-  margin: '10px 0 20px 0',
-  boxShadow: '0 0 1px 1px rgba(0,0,0,.3)',
+  textAlign: 'center',
+  border: '1px solid black',
+  borderRadius: 5,
+  marginTop: '5px'
 })
 
 const Label = styled('div')({
-  fontSize: 14
+  fontSize: 20
 })
 
 const BudgetItem = ({ label, onChangeFn, name, value }) => {
   return (
-    <div>
+    <BudgetContent>
       <Label>{label}</Label>
       <CategoryInput
         name={name}
         value={value}
         onChange={onChangeFn}
       />
-    </div>
+    </BudgetContent>
   )
 }
 
