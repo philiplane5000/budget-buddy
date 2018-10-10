d51443c17ea9e582ccb327cc406708fb04e7ee55
nickfp1985 <nfpeddie@gmail.com>
2018-10-09 21:03:42 -0700

    Nigel & Nick reworked the components and pages to make them more uniform with each other. Still working on a finished frontend look

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,72 +1,35 @@
 import React from "react";
 import styled from "react-emotion";
 import { Link } from "react-router-dom";
+import Button from "../components/Button";
+import Header from "../components/Header";
 
-const ButtonBudget = styled("button")({
-  backgroundColor: "#1162bc",
-  fontSize: 30,
-  color: "white",
-  borderRadius: 45,
-  margin: 10,
-  height: 50,
-  width: 250,
-  fontFamily: "Calibri, Candara, Segoe, Segoe UI, Optima, Arial, sans-serif"
-});
+const MainWrapper = styled('div')(
+  {
+    border: '1px solid #092d63',
+    padding: 40,
+    borderRadius: 10,
+    boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);',
+    backgroundColor: '#d5d6d2'
+  }
+)
 
-const ButtonPurchase = styled("button")({
-  backgroundColor: "#1162bc",
-  fontSize: 30,
-  color: "white",
-  borderRadius: 45,
-  margin: 10,
-  height: 50,
-  width: 250,
-  fontFamily: "Calibri, Candara, Segoe, Segoe UI, Optima, Arial, sans-serif"
-});
-
-const Content = styled("div")({
-  height: 200,
-  width: 400,
-  backgroundColor: "#2fc4a6",
-  margin: "0 auto",
-  marginTop: 50,
-  marginBottom: 20,
-  borderRadius: 10
-});
-
-const Header = styled("div")({
-  textAlign: "center",
-  fontFamily: "Calibri, Candara, Segoe, Segoe UI, Optima, Arial, sans-serif",
-  color: 'white',
-  margin: '0 auto',
-  justifyContent: 'center',
-  alignItems: 'center'
-});
-
-const Wrapper = styled("div")({
-  height: 400
-});
 
 const Main = () => {
   return (
-    <Wrapper>
-        <Content>
-          <Header>
-            <h2>You're on track!</h2>
-            <h2>You've spent $1500 so far!</h2>
-          </Header>
-        </Content>
-      <ButtonPurchase>
+    <MainWrapper>
+      <Header />
+      <Button>
         <Link to="/purchase" style={{ textDecoration: "none", color: "white" }}>
           Make a Purchase
         </Link>
-      </ButtonPurchase>
-      <ButtonBudget>
+      </Button>
+      <Button>
         <Link to="/budget" style={{ textDecoration: "none", color: "white" }}>
           Edit Budget
         </Link>
-      </ButtonBudget>
-    </Wrapper>
+      </Button>
+    </MainWrapper>
   );
 };
 
