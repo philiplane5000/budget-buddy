bae57fcd521cf3ab0acd6831aafaf0f34b5b8f82
philtd5000 <philiptaft9@gmail.com>
2018-10-13 13:06:48 -0700

    footer fix

--- client/src/pages/Articles.js
+++ client/src/pages/Articles.js
@@ -15,58 +15,61 @@ class Articles extends Component {
         articles: []
     }
 
-    usNews = () =>  {
+    usNews = () => {
         API.findUSNews().then(articles => {
-            this.setState({articles: articles.data})
+            this.setState({ articles: articles.data })
         })
     }
 
-    todayNews = () =>  {
+    todayNews = () => {
         API.findToday().then(articles => {
-            this.setState({articles: articles.data})
+            this.setState({ articles: articles.data })
         })
     }
 
-    streetNews = () =>  {
+    streetNews = () => {
         API.findStreet().then(articles => {
-            this.setState({articles: articles.data})
+            this.setState({ articles: articles.data })
         })
     }
 
     render() {
         return (
-            <Grid container justify="center">
+            <div>
+            <Grid container justify="center" style={{ marginBottom: 100 }}>
 
                 <Grid item lg={8} md={10} sm={10} xs={10}>
                     <Wrapper>
-                        <Header headerText="Staying on Track"/>
+                        <Header headerText="Staying on Track" />
 
                         <Button onClick={this.usNews}>
-                                US News
+                            US News
                          </Button>
 
-                         <Button  onClick={this.streetNews}>
-                                The Street
+                        <Button onClick={this.streetNews}>
+                            The Street
                          </Button>
 
-                         <Button  onClick={this.todayNews}>
-                                USA Today
+                        <Button onClick={this.todayNews}>
+                            USA Today
                          </Button>
 
-                    {this.state.articles.length > 0 ? (
-                        this.state.articles.map(article=>(
-                            <Article headline={article.title} link={article.link} excerpt={article.excerpt}></Article>
-                        ))
-                    ) : (
-                        <p>Read up on how to manage your budget and perfect your personal financing.</p>
-                    )
-                
-                }
+                        {this.state.articles.length > 0 ? (
+                            this.state.articles.map(article => (
+                                <Article headline={article.title} link={article.link} excerpt={article.excerpt}></Article>
+                            ))
+                        ) : (
+                                <p>Read up on how to manage your budget and perfect your personal financing.</p>
+                            )
+
+                        }
 
                     </Wrapper>
                 </Grid>
-                <Footer />
             </Grid>
+            <Footer />
+            </div>
+
         )
     }
 }
