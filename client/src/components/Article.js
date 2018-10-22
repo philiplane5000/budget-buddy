b49a225204445d092dcdc0774d66638b12556c70
Rico <enriquelopez80@gmail.com>
2018-10-22 14:56:59 -0700

    article styling

--- client/src/components/Article.js
+++ client/src/components/Article.js
@@ -20,6 +20,11 @@ const ArticleWrapper = styled('div')`
     text-align: left;
     flex: 85%;
     margin: 10px 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
 `
 
 const Link = styled('a')`
@@ -48,17 +53,18 @@ const Text = styled('h2')`
 const Save = styled('button')`
     cursor: pointer;
     color: white;
-    margin: 0px 0px 0px 120px;
+    max-width: auto;
     background-color: #1162bc;
     font-family: 'Roboto', sans-serif;
-    border: solid 03px #1162bc;
     border-radius: 05px;
+    border: none;
     outline: none;
-    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+    box-shadow: 0 05px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
     transition: all .3s ease-in-out;
 
     :hover {
-        transform: scale(1.10)
+        transform: scale(1.10);
+        background-color: #3de28c;
     }
 `
 
@@ -90,7 +96,7 @@ const Article = props => (
             </Text>
 
 
-            <Save style={{ margin: '0px 0px 0px 130px'}} 
+            <Save 
                 title={props.title}
                 excerpt = {props.excerpt}
                 link = {props.link}
